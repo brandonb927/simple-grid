c38ca8873487461e324de2e8a222a83ae1e3773c
Brandon Brown <brandon@brandonbrown.io>
2014-02-18 15:02:53 -0800

    Convert to 2-space tabbing

--- gruntfile.js
+++ gruntfile.js
@@ -1,71 +1,71 @@
 module.exports = function(grunt) {
-    'use strict';
+  'use strict';
 
-    // Force use of Unix newlines
-    grunt.util.linefeed = '\n';
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
 
-    grunt.initConfig({
-        pkg : grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg : grunt.file.readJSON('package.json'),
 
-        banner: '/*!\n' +
-                ' * Simple-Grid v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-                ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-                ' * Licensed under <%= _.pluck(pkg.licenses, "type") %> (<%= _.pluck(pkg.licenses, "url") %>)\n' +
-                ' */\n',
+    banner: '/*!\n' +
+            ' * Simple-Grid v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= _.pluck(pkg.licenses, "type") %> (<%= _.pluck(pkg.licenses, "url") %>)\n' +
+            ' */\n',
 
-        // Directory variables
-        lessDir : 'less/',
-        cssDir  : 'css/',
+    // Directory variables
+    lessDir : 'less/',
+    cssDir  : 'css/',
 
-        // Run LESS CSS compilation
-        less: {
-            compile: {
-                files: {
-                    "<%= cssDir %><%= pkg.name %>.css": "<%= lessDir %><%= pkg.name %>.less"
-                }
-            },
-            minify: {
-                options: {
-                    cleancss: true,
-                    report: 'min'
-                },
-                files: {
-                    'css/<%= pkg.name %>.min.css': 'css/<%= pkg.name %>.css',
-                }
-            }
-        },
-
-        usebanner: {
-            options: {
-                position: 'top',
-                banner: '<%= banner %>'
-            },
-            files: {
-                src: [
-                    'css/<%= pkg.name %>.css',
-                    'css/<%= pkg.name %>.min.css',
-                ]
-            }
-        },
-
-        // Watch for changes on these files
-        watch: {
-            options: {
-                nospawn: true
-            },
-            less: {
-                files: ['<%= lessDir %>**/*.less'],
-                tasks: ['less']
-            }
+    // Run LESS CSS compilation
+    less: {
+      compile: {
+        files: {
+          "<%= cssDir %><%= pkg.name %>.css": "<%= lessDir %><%= pkg.name %>.less"
         }
-    })
+      },
+      minify: {
+        options: {
+          cleancss: true,
+          report: 'min'
+        },
+        files: {
+          'css/<%= pkg.name %>.min.css': 'css/<%= pkg.name %>.css',
+        }
+      }
+    },
+
+    usebanner: {
+      options: {
+        position: 'top',
+        banner: '<%= banner %>'
+      },
+      files: {
+        src: [
+          'css/<%= pkg.name %>.css',
+          'css/<%= pkg.name %>.min.css',
+        ]
+      }
+    },
+
+    // Watch for changes on these files
+    watch: {
+      options: {
+        nospawn: true
+      },
+      less: {
+        files: ['<%= lessDir %>**/*.less'],
+        tasks: ['less']
+      }
+    }
+  })
 
 
-    // Grunt plugins
-    grunt.loadNpmTasks('grunt-banner');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  // Grunt plugins
+  grunt.loadNpmTasks('grunt-banner');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Registered Grunt tasks
-    grunt.registerTask('default', ['less', 'usebanner', 'watch']);
+  // Registered Grunt tasks
+  grunt.registerTask('default', ['less', 'usebanner', 'watch']);
 }
